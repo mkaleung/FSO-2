a72463a0dd1f6e6e2282c1aebb35d85be03ea43a
jincrypt <leung.mka@gmail.com>
2023-02-24 10:29:43 -0500

    exercise 2.9 - included filter function (case insensitive)

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -11,15 +11,16 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567',
-    },
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  
+  const [filterName, setFilterName] = useState('')
+
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) {
@@ -28,7 +29,8 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length +  1,
     }
     
     setPersons(persons.concat(personObject))
@@ -44,9 +46,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value)
+  }
+
+  const contactsToShow = (filterName) => {
+    if (!filterName) {
+      return persons
+    } else {
+      return persons.filter((person) => {
+        return person.name.toLowerCase().includes(filterName)
+      })
+    }
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter: <input value={filterName} onChange={handleFilterChange} />
+      </div>
+      <h2>Add New Contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -60,7 +80,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {contactsToShow(filterName).map(person => 
           <Person key={person.name} person={person} />
         )}
       </ul>
